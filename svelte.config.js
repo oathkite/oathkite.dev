00b7dcd52dbf38545c2917cfff4f5674248141d8
oathkite <kita@impv.co.jp>
2022-10-30 18:04:06 +0900

    misc: デプロイ周りの設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
+import adapter from '@sveltejs/adapter-auto';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
+        adapter: adapter(),
         files: {
             assets: "src/assets"
         },
