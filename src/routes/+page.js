714befa72019f7b4c18531c8647a522771f06d77
oathkite <kita@impv.co.jp>
2022-11-01 02:58:22 +0900

    一覧の取得数を48に変更

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
 import { PUBLIC_API_KEY } from '$env/static/public';
 
 export async function load({ fetch }) {
-    const post = await fetch(`https://oathkite-dev.microcms.io/api/v1/work`, {
+    const post = await fetch(`https://oathkite-dev.microcms.io/api/v1/work?limit=48`, {
         headers: {
             'X-MICROCMS-API-KEY': PUBLIC_API_KEY,
         }
